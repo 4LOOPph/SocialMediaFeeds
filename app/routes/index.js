99c0f833d4ef3cd57d396004d692b43e94ebd4f4
Philip Cesar Garay <philipgaray2@gmail.com>
2017-01-16 01:41:18 +0800

    Initial Commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,77 @@
 
 'use strict';
 
-module.exports = function(app,config, middleware) {
+var ig = require('instagram-node').instagram();
+module.exports = function(app, config, middleware) {
+
+    var redirect_uri = config.api_host_url + '/api/handleauth';
 
     app.route('/')
         .get(function onRequest(req, res) {
             res.render('index');
         });
+
+    app.route('/twitter')
+        .get(function onRequest(req, res) {
+            res.render('twitter');
+        });
+
+    app.route('/instagram')
+        .get(function onRequest(req, res) {
+            res.render('instagram');
+        });
+
+    app.route('/facebook')
+        .get(function onRequest(req, res) {
+            res.render('facebook');
+        });
+
+    app.route('/api/twitter-feeds')
+        .get(function onRequest(req, res) {
+            var Twitter = require('twitter');
+
+            var client = new Twitter({
+                consumer_key: config.TWITTER_KEY,
+                consumer_secret: config.TWITTER_SECRET,
+                access_token_key: config.TWITTER_OAUTH_ACCESS_TOKEN,
+                access_token_secret: config.TWITTER_OAUTH_ACCESS_TOKEN_SECRET
+            });
+
+            client.get('search/tweets', { q: 'engagisxsm' }, function(error, tweets, response) {
+                if (error) {
+                    return res.status(500).json({ response: error, statusCode: 500 });
+                }
+
+                res.status(200).json({ response: tweets, statusCode: 200 });
+            });
+        });
+
+    app.route('/api/facebook-feeds')
+        .get(function onRequest(req, res) {
+
+        });
+
+    app.route('/api/instagram-feeds')
+        .get(function onRequest(req, res) {
+            ig.use({
+                client_id: config.INSTAGRAM_ID,
+                client_secret: config.INSTAGRAM_SECRET
+            });
+
+            res.redirect(ig.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+        });
+
+    app.route('/api/handleauth')
+        .get(function onRequest(req, res) {
+            ig.authorize_user(req.query.code, redirect_uri, function(err, result) {
+                if (err) {
+                    console.log(err.body);
+                    res.send("Didn't work");
+                } else {
+                    console.log('Yay! Access token is ' + result.access_token);
+                    res.send('You made it!!');
+                }
+            });
+        });
+
 };
--- config/environment/development.js
+++ config/environment/development.js
@@ -17,9 +17,13 @@ module.exports = {
     api_version: process.env.API_VERSION || '/api/1.0',
     sync_version: process.env.API_VERSION || '/sync/1.0',
     app_version: process.env.APP_VERSION || '/1.0',
-    FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || '265f79c8427710554809f046befe7385',
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'O6WEPCL11RqD4uq6pGG8cHo5',
-    LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'evc6YfOENjeUfzx0',
-    TWITTER_KEY: process.env.TWITTER_KEY || 'N2PvDrRCy71AKGV22qQQut3QG',
-    TWITTER_SECRET: process.env.TWITTER_SECRET || 'kFhteRSJmVPUV95aWAwKJJKJRDZDW27y2xNRacGNE5iXD9j67H'
+    FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || '',
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET || '',
+    LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || '',
+    INSTAGRAM_ID: process.env.LINKEDIN_SECRET || '366fbd90b61944ebad2bcedd5dbd9392',
+    INSTAGRAM_SECRET: process.env.LINKEDIN_SECRET || '97390ed1a9854aa396b37c9413017bd7',
+    TWITTER_KEY: process.env.TWITTER_KEY || 'E4z4u14GI84cyDQOQz0UN2odO',
+    TWITTER_SECRET: process.env.TWITTER_SECRET || 'ibIcUQl3vzqJ4ETqoHP7d0OvfBT4KLy9KrkgvgIkkpwESAx8YJ',
+    TWITTER_OAUTH_ACCESS_TOKEN : process.env.TWITTER_OAUTH_ACCESS_TOKEN || '1459616382-LgIk3VRzoxOh3ATvUfVCOOazcziZ5zBrVlHpU2B',
+    TWITTER_OAUTH_ACCESS_TOKEN_SECRET: process.env.TWITTER_OAUTH_ACCESS_TOKEN_SECRET || 'SuCOHWuyklr1vKqHBVpZB6gIrvKKH8AuQCHP0sugvxrD5'
 };
